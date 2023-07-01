ed0acfd81d271da622731d38700f66e3ae509a57
Daneric <danericaalbers@hotmail.com>
2023-07-01 18:27:17 +0200

    refactor: now i can get data from the api

--- src/App.vue
+++ src/App.vue
@@ -2,56 +2,66 @@
   <div>
     <div v-if="!playerSearched" class="searchPlayer">
       <input type="text" v-model="inputValue" placeholder="Enter a value" />
-      <button @click="submitRequest">Submit</button>
+      <button @click="getPlayer">Submit</button>
     </div>
     <div v-if="isLoading">Loading...</div>
     <div v-else-if="error">{{ error }}</div>
-    <div v-else-if="responseData">{{ responseData }}</div>
+    <!-- <div v-else-if="responseData">{{ responseData }}</div> -->
+    <div v-if="playerSearched" class="dataContainer">
+      <h1>Username: {{ data.username }}</h1>
+      <h1>First Join: {{ data.meta.firstJoin }}</h1>
+      <h1>Online: {{ data.meta.location.online }}</h1>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue';
-
-export default {
-  setup() {
-    const inputValue = ref('');
-    const isLoading = ref(false);
-    const error = ref('');
-    const responseData = ref('');
-    const playerSearched = ref(false);
-
-    const submitRequest = async () => {
-      isLoading.value = true;
-      error.value = '';
-      responseData.value = '';
-
-      try {
-        const response = await fetch(`https://api.wynncraft.com/v2/player/${inputValue.value}/stats`);
-        const data = await response.json();
-        if(data.code === 400) {
-          error.value = 'Player not found';
-        } else if(data.code === 200) {
-          playerSearched.value = true;
-          responseData.value = JSON.stringify(data);
-        } else {
-          error.value = 'An error occurred. Please try again.';
-        }
-      } catch (error: any) {
-        error.value = 'An error occurred. Please try again.';
-      } finally {
-        isLoading.value = false;
-      }
-    };
-
-    return {
-      inputValue,
-      isLoading,
-      error,
-      playerSearched,
-      responseData,
-      submitRequest
-    };
+interface playerData {
+  username: string;
+  uuid: string;
+  meta: {
+    firstJoin: number;
+    lastJoin: number;
+    location: {
+      online: boolean;
+      server: string;
+    }
   }
+}
+export default {
+  data() {
+    return {
+      inputValue: '',
+      isLoading: false,
+      error: '',
+      responseData: '',
+      playerSearched: false,
+      data: {} as playerData,
+    };
+  },
+  methods: {
+    getPlayer() {
+      fetch(`https://api.wynncraft.com/v2/player/${this.inputValue}/stats`)
+        .then((response) => response.json())
+        .then((data) => {
+          if(data.code === 400) {
+            this.error = 'Player not found';
+          } else if(data.code === 200) {
+            this.playerSearched = true;
+            this.data = data.data[0];
+            console.log(data.data[0]);
+          } else {
+            this.error = 'An error occurred. Please try again.';
+          }
+        })
+        .catch((error) => {
+          this.error = 'An error occurred. Please try again.';
+          console.log(error);
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    }
+  },
 };
 </script>
