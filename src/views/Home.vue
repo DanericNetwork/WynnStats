e5107b1588be8d52fc8c2e8726c0c2295079f8cf
Daneric <danericaalbers@hotmail.com>
2023-07-02 14:47:01 +0200

    refactor: everything has its own page now

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-if="!playerSearched" class="searchPlayer-container">
+            <div class="searchPlayer">
+            <input type="text" v-model="inputValue" placeholder="Playername" @keyup.enter="getPlayer" />
+            <button @click="getPlayer">Search player</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Home',
+  data() {
+    return {
+      inputValue: '',
+      playerSearched: false,
+    };
+  },
+  methods: {
+    getPlayer() {
+      if (this.inputValue.length > 0) {
+        this.$router.push(`/player/${this.inputValue}`);
+        this.playerSearched = true;
+      }
+    },
+  },
+};
+</script>
