70a705fd7894843f68ed0c90197831f62b7a01cd
Daneric <danericaalbers@hotmail.com>
2023-07-02 14:19:51 +0200

    chore: removed unused import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify';
+import Vue3Toasity from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 
 createApp(App).use(
